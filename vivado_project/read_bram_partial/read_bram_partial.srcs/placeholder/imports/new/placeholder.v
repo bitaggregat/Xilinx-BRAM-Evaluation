17641e6892162a819c8813246e570d3b4ffbf1e9
Wakiya Schulz <w.schulz@bitaggregat.de>
2024-07-23 13:16:45 +0200

    Added vivado project build script and sources

---
+++ vivado_project/read_bram_partial/read_bram_partial.srcs/placeholder/imports/new/placeholder.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/08/2024 01:43:39 PM
+// Design Name: 
+// Module Name: placeholder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module placeholder (
+    input clka,
+    input [9:0] addra,
+    output [35:0] douta
+    );
+    
+    assign douta = 36'b11;
+
+endmodule
---
+++ vivado_project/read_bram_partial/read_bram_partial.srcs/sources_1/imports/new/return_0.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/08/2024 02:02:22 PM
+// Design Name: 
+// Module Name: return_0
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module return_0 (
+    input clka,
+    input [13:0] addra,
+    output [8:0] douta
+    );
+    
+    assign douta = 9'b000000000;
+
+endmodule
