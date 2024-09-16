9053f72e830dc871b2b98a866bb8cc5b0efeed93
Wakiya Schulz <w.schulz@bitaggregat.de>
2024-09-16 14:07:19 +0200

    Added script that allows reading multiple pblocks using config files

---
+++ run_pblock_analysis.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Help output:
+read -r -d '' help <<EOM
+Meta Script that calls "run_bram_analysis.sh" over multiple pblocks
+Expects a config file for each pblock.
+The config file should be a bash script that defines variables in the following form:
+    vivado_path="~/my/vivado/path"
+    vivado_project_path="~/my/project/path"
+    pblock="pblock_name"
+    bram_row_x_position=10
+    bram36_min_y_position=10
+    bram36_max_y_position=17
+    reads=1000
+    output_path="~/my/output/path"
+
+NOTE: Do not ever pass randomly scripts as arguments to this script. (arbitrary code exec)
+EOM
+
+# "Parse" arguments
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    echo "$help"
+    exit 0
+fi
+
+for config_file in $(seq "1" "$#"); do
+    source "${config_file}"
+    source run_bram_analysis "${vivado_path}" "${vivado_project_path}" "${pblock}" "${bram_row_x_position}" "${bram36_min_y_position}" "${bram36_max_y_position}" "${reads}" "${output_path}"
+done
