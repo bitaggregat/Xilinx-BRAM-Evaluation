59e6dd5b49fec1f1cb24b6a9c28092c7b6e28d65
Wakiya Schulz <w.schulz@bitaggregat.de>
2024-09-16 13:39:36 +0200

    Added loops over bram blocks, pblock and bitstream files to hdf5 converter script

--- create_hdf5_from_file_structure.py
+++ create_hdf5_from_file_structure.py
@@ -38,7 +38,7 @@ def add_temperature_dataset(temperature_file: Path, parent: h5py._hl.group.Group
         parent.create_dataset("temperature" ,values, dtype=f)
 
 
-def add_bram_read_dataset(path: Path, parent: h5py._hl.group.Group, name: str, binary: bool = False) -> None:
+def add_bram_dataset_group(path: Path, parent: h5py._hl.group.Group, group_name: str, binary: bool = False) -> None:
     '''
     Adds bram reads as a dataset to <bram_name>/<0-to-f or f-to-0> group
 
@@ -48,13 +48,14 @@ def add_bram_read_dataset(path: Path, parent: h5py._hl.group.Group, name: str, b
         name: Name of the dataset (should be either "data" or "parity")
         binary: Is read saved in binary or ASCII?
     '''
-    
+    bram_data_group = parent.create_group(group_name)
+
     # Sort files numerically ascending: [0, 1, 2, ..., 10, ...]
     # This is done to avoid: [0, 1, 10, 100, 1000, 2, 20,...] 
     files_sorted_by_index = sorted(
         [
             file_path for file_path in path.iterdir()
-            if file_path.is_file()
+            if file_path.is_file() and file_path.name != "temperature.txt"
         ],
         key=lambda x: int(x.stem)
     )
@@ -68,27 +69,87 @@ def add_bram_read_dataset(path: Path, parent: h5py._hl.group.Group, name: str, b
     # Quality control
     assert all([len(read) == seq_length for read in read_data])
 
-    parent.create_dataset(name, read_data, dtype=h5py.string_dtype(length=seq_length))
+    bram_data_group.create_dataset("measurements", read_data, dtype=h5py.string_dtype(length=seq_length))
+
+    # Add temperature if available
+    temperature_path = Path(path, "temperature.txt") 
+    if temperature_path.exists() and temperature_path.is_file():
+        add_temperature_dataset(Path(path, "temperature.txt"), bram_data_group)
 
 
+def add_bitstream_group(path: Path, parent: h5py._hl.group.Group) -> None:
 
-def add_bram_group(path: Path, parent: h5py._hl.group.Group) -> None:
+    bs_files = [
+        file_path for file_path in path.iterdir()
+        if file_path.is_file()
+    ]
+
+    bitstream_group = parent.create_group("bitstreams")
+
+    # Add five bitstreams as attributes
+    for bs in bs_files:
+        match bs.stem.split("_"):
+            case [*_, "partial", "bram", "bs"]:
+                with open(bs, mode="rb") as f:
+                    bitstream_group.attrs["partial_bram_bs"] = f.read()
+            case [*_, "modified", "partial"]:
+                with open(bs, mode="rb") as f:
+                    bitstream_group.attrs["modified_partial"] = f.read()
+            case [*_, "ff"]:
+                with open(bs, mode="rb") as f:
+                    bitstream_group.attrs["ff"] = f.read()
+            case [*_, "00"]:
+                with open(bs, mode="rb") as f:
+                    bitstream_group.attrs["00"] = f.read()
+            case [*_, "bramless", "partial"]:
+                with open(bs, mode="rb") as f:
+                    bitstream_group.attrs["bramless_partial"] = f.read()
+    
+
+def add_bram_group(path: Path, parent: h5py._hl.group.Group, binary: bool = False) -> None:
     '''
     Adds measurement Data from single BRAM experiment as path to a given parent group
+    - Adds measurements with previous value ff and 00 if available)
+    - Also adds bitstreams if available
 
     Parameters:
         path: Path to BRAM measurements in (Linux)filesystem
         parent: Parent group of new group
+        binary: True if measurements were done in binary
     '''
 
     bram_name = path.parts[-1]
 
-    parent.create_group(bram_name)
+    bram_group = parent.create_group(bram_name)
 
-    # Create temperature dataset
-    temperature_path = Path(path, "temperature.txt") 
-    if temperature_path.exists() and temperature_path.is_file():
-        pass
+    expected_paths = [
+        Path(path, "previous_value_ff"),
+        Path(path, "previous_value_00")
+    ]
+    
+    for expected_path in expected_paths:
+        if expected_path.exists() and expected_path.is_dir():
+            add_bram_dataset_group(expected_path, bram_group, expected_path.parts[-1], binary)
+    
+    bs_path = Path(path, "bs")
+    if bs_path.exists() and bs_path.is_dir():
+        add_bitstream_group(bs_path, bram_group)
+
+def add_pblock_group(path: Path, parent: h5py._hl.group.Group, binary: bool = False) -> None:
+    '''
+    Adds all bram data directories from a given pblock directory
+    '''
+
+    pblock_name = path.parts[-1]
+    pblock_group = parent.create_group(pblock_name)
+
+    bram_dirs = [
+        sub_path for sub_path in path.iterdir()
+        if sub_path.is_dir() and "RAMB" in sub_path.parts[-1]
+    ]
+
+    for bram_dir in bram_dirs:
+        add_bram_group(bram_dir, pblock_group, binary)
 
 parser = argparse.ArgumentParser("Script converts read bram data structured in directories, to hdf5")
 parser.add_argument(
@@ -111,16 +172,32 @@ parser.add_argument(
     "-u", "--uart_sn",
     help="Serial number of uart adapter that was used to read bram"
 )
+parser.add_argument(
+    "-b", "--binary_files",
+    help="Read data was saved in binary files.",
+    action="store_true"
+)
 
 if __name__ == "__main__":
     args = parser.parse_args()
     date = str(datetime.datetime())
     with open(f"{args["root_dir"]}_{date}.hdf5", "w") as f:
         root_group = f.create_group("base")
-
+        
         meta_data = vars(args)
+        root_path = Path(meta_data["root_dir"])
+        use_binary_files = meta_data["binary_fils"]
         meta_data.pop("root_dir")
+        meta_data.pop("binary_files")
 
         add_meta_data(root_group, meta_data)
 
+        # Iterate through all pblocks in given base directory
+        pblock_dirs = [
+            sub_path for sub_path in root_path.iterdir()
+            if sub_path.is_dir() and "pblock" in sub_path.parts[-1]
+        ]
+
+        for pblock_dir in pblock_dirs:
+            add_pblock_group(pblock_dir, root_group, use_binary_files)
         
