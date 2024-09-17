ceb8c6229331bc379b3ffed0af2de6bfe5c291e3
Wakiya Schulz <w.schulz@bitaggregat.de>
2024-09-17 10:12:01 +0200

    Added example configuration file

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+vivado_path="/tools/Xilinx/Vivado/2024.1/bin/vivado"
+vivado_project_path="/home/kiya/Repos/xilinx-bram-evaluation/vivado_project/te0802/read_bram_te0802"
+pblock="pblock_1"
+bram_row_x_position=2
+bram36_min_y_position=12
+bram36_max_y_position=12
+reads=1
+output_path="test_runs/temp_pblock1"
