9594fb5e1b43adcdd28982cdf12de803a7bdf344
Wakiya Schulz <w.schulz@bitaggregat.de>
2024-09-17 10:12:59 +0200

    Fixed previously overseen off-by-1 bug

--- run_bram_analysis.sh
+++ run_bram_analysis.sh
@@ -146,7 +146,7 @@ for current_bram_y_position in $(seq "$bram36_min_y_position" "$bram36_max_y_pos
     fi
 
     # With previous value 00:
-    for read in $(seq 1 "$reads"); do
+    for read in $(seq 0 "$reads"); do
         # BRAM init
         flash_bitstreams "${full_bs_with_initial_value_00}" "${bramless_partial_bs}" "${from_root}/${modified_bs}";
         # Readout process
