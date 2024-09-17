f703b13c0939c8a79da1f4f69254daec926773c8
Wakiya Schulz <w.schulz@bitaggregat.de>
2024-09-17 11:07:27 +0200

    Added vivado temperature measurement test

---
+++ tests/how_to_run_tests.md
@@ -0,0 +1,16 @@
+# Tests
+
+Testing will be/has been done sparsely in this project.
+A small test is created whenever a logical error occurs (not small syntax errors).
+
+## Prerequisites
+
+Set your path variables in test_config.sh.
+
+## Execution
+
+**PLEASE MAKE SURE TO RUN THE SCRIPT FROM THE REPOSITORIES BASE DIRECTORY.**
+
+```bash
+tests/run_tests.sh tests/test_config.sh
+```
---
+++ tests/run_tests.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Wrong number of arguments ${#}!"
+    exit 0
+else
+    source $1
+fi
+
+#######################################
+# Temperature test
+# Sometimes run_bram_analysis.sh tried to read temperature from tmux
+#   before it was displayed by vivado
+#######################################
+source run_pblock_analysis.sh "${vivado_path}" "${vivado_project_path}" "pblock_1" 2 12 12 100 "tests/temp_dir"
+
+# Verify content of temperature files
+while IFS= read -r line || [[ -n "$line" ]]; do
+    if [ ! ${#line} -gt 9 ] || [[ ! $line =~ ^[0-9\.]+$ ]]; then
+        echo "Temperature test failed"
+        exit 0
+    fi
+done < "tests/temp_dir/pblock_1/previous_value_ff/temperature.txt"
+
+while IFS= read -r line || [[ -n "$line" ]]; do
+    if [ ! ${#line} -gt 9 ] || [[ ! $line =~ ^[0-9\.]+$ ]]; then
+        echo "Temperature test failed"
+        exit 0
+    fi
+done < "tests/temp_dir/pblock_1/previous_value_00/temperature.txt"
