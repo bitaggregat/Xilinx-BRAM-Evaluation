948a9d6d54bc6491aeaee2500ce6c344d8c5c3be
Wakiya Schulz <w.schulz@bitaggregat.de>
2024-09-17 11:35:19 +0200

    fixed bugs in run_tests.sh temperature string check

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -12,19 +12,23 @@ fi
 # Sometimes run_bram_analysis.sh tried to read temperature from tmux
 #   before it was displayed by vivado
 #######################################
-source run_pblock_analysis.sh "${vivado_path}" "${vivado_project_path}" "pblock_1" 2 12 12 100 "tests/temp_dir"
+#source run_bram_analysis.sh "${vivado_path}" "${vivado_project_path}" "pblock_1" 2 12 12 100 "tests/temp_dir"
 
 # Verify content of temperature files
 while IFS= read -r line || [[ -n "$line" ]]; do
-    if [ ! ${#line} -gt 9 ] || [[ ! $line =~ ^[0-9\.]+$ ]]; then
+    if [ ${#line} -gt 9 ] || [[ ! $line =~ ^[0-9\.[:space:]]+$ ]]; then
+        echo $line
         echo "Temperature test failed"
         exit 0
     fi
-done < "tests/temp_dir/pblock_1/previous_value_ff/temperature.txt"
+done < "tests/temp_dir/pblock_1/RAMB36_X2Y12/previous_value_ff/temperature.txt"
 
 while IFS= read -r line || [[ -n "$line" ]]; do
-    if [ ! ${#line} -gt 9 ] || [[ ! $line =~ ^[0-9\.]+$ ]]; then
+    if [ ${#line} -gt 9 ] || [[ ! $line =~ ^[0-9\.[:space:]]+$ ]]; then
+        echo $line
         echo "Temperature test failed"
         exit 0
     fi
-done < "tests/temp_dir/pblock_1/previous_value_00/temperature.txt"
+done < "tests/temp_dir/pblock_1/RAMB36_X2Y12/previous_value_00/temperature.txt"
+
+echo "Tests succeeded"
