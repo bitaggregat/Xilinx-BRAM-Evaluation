be3b1457f1a228b97422a26f2e6c5c982b3bc395
Wakiya Schulz <w.schulz@bitaggregat.de>
2024-11-04 11:31:10 +0100

    Reformatted more files using Ruff

--- chi2_over_bram_block.py
+++ chi2_over_bram_block.py
@@ -11,6 +11,7 @@ import h5py
 
 from hdf5_wrapper import Experiment, ReadSession, Read
 
+
 @dataclass
 class ContingencyTable:
     variables: List[str]
@@ -19,13 +20,15 @@ class ContingencyTable:
 
     def to_list(self) -> List[List[int]]:
         return [self.zero_count_list, self.one_count_list]
-    
+
     def percentage_flipped_dict(self) -> Dict[str, float]:
         variable_to_percentage_dict = dict()
-        for zero_count, one_count, variable in zip(self.zero_count_list, self.one_count_list, self.variables):
-            variable_to_percentage_dict[variable] = one_count/(zero_count + one_count)
+        for zero_count, one_count, variable in zip(
+            self.zero_count_list, self.one_count_list, self.variables
+        ):
+            variable_to_percentage_dict[variable] = one_count / (zero_count + one_count)
         return variable_to_percentage_dict
-    
+
     def contingency_odds(self) -> float:
         if len(self.variables) > 2:
             raise Exception("Can't calculate contingency odds on more than 2 values")
@@ -33,24 +36,27 @@ class ContingencyTable:
 
 
 def unpack_from_hdf5(path: Path) -> Experiment:
-    '''
+    """
     Opens hdf5 and converts it to Experiment hdf5 wrapper class
-    '''
+    """
     with h5py.File(path, "r") as f:
         experiment = Experiment.from_hdf5(f, f.attrs["commit"])
 
     return experiment
 
-def normalize_frequency_lists(zero_count_list: List[int], one_count_list: List[int]) -> Tuple[List[int], List[int]]:
+
+def normalize_frequency_lists(
+    zero_count_list: List[int], one_count_list: List[int]
+) -> Tuple[List[int], List[int]]:
     # TODO Review this solution to observed 0 frequency
     new_zero_count_list = []
     new_one_count_list = []
     for idx in range(len(zero_count_list)):
         if zero_count_list[idx] == 0:
             new_zero_count_list.append(1)
-            new_one_count_list.append(one_count_list[idx]-1)
+            new_one_count_list.append(one_count_list[idx] - 1)
         elif one_count_list[idx] == 0:
-            new_zero_count_list.append(zero_count_list[idx]-1)
+            new_zero_count_list.append(zero_count_list[idx] - 1)
             new_one_count_list.append(1)
         else:
             new_zero_count_list.append(zero_count_list[idx])
@@ -59,16 +65,17 @@ def normalize_frequency_lists(zero_count_list: List[int], one_count_list: List[i
     return new_zero_count_list, new_one_count_list
 
 
-def contingency_table_list(read_sessions: Dict[str, ReadSession], bit_type: str, normalize: bool) -> List[ContingencyTable]:
+def contingency_table_list(
+    read_sessions: Dict[str, ReadSession], bit_type: str, normalize: bool
+) -> List[ContingencyTable]:
     """
     bit_type: either "parity_reads" or "data_reads"
     """
 
     contingency_table_list = list()
-    for byte_idx in range(len(
-        getattr(read_sessions[list(read_sessions.keys())[0]], bit_type)[0].bits
-        )):
-        
+    for byte_idx in range(
+        len(getattr(read_sessions[list(read_sessions.keys())[0]], bit_type)[0].bits)
+    ):
         for bit_idx in range(8):
             variables = list()
             zero_count_list = list()
@@ -76,25 +83,32 @@ def contingency_table_list(read_sessions: Dict[str, ReadSession], bit_type: str,
 
             for variable_name, read_session in read_sessions.items():
                 variables.append(variable_name)
-                one_count = sum([
-                    read.bits[byte_idx][bit_idx] 
-                    for read in getattr(read_session, bit_type)
-                ])
+                one_count = sum(
+                    [
+                        read.bits[byte_idx][bit_idx]
+                        for read in getattr(read_session, bit_type)
+                    ]
+                )
                 zero_count = len(getattr(read_session, bit_type)) - one_count
-                
+
                 zero_count_list.append(zero_count)
                 one_count_list.append(one_count)
-            
+
             # TODO Review this solution to observed 0 frequency
             if normalize:
-                zero_count_list, one_count_list = normalize_frequency_lists(zero_count_list, one_count_list)
-                    
-            contingency_table_list.append(ContingencyTable(
-                variables=variables,
-                zero_count_list=zero_count_list,
-                one_count_list=one_count_list
-            ))
+                zero_count_list, one_count_list = normalize_frequency_lists(
+                    zero_count_list, one_count_list
+                )
+
+            contingency_table_list.append(
+                ContingencyTable(
+                    variables=variables,
+                    zero_count_list=zero_count_list,
+                    one_count_list=one_count_list,
+                )
+            )
     return contingency_table_list
+
 
 def chi2_result_list(contingency_tables: List[ContingencyTable]) -> list:
     chi2_results = list()
@@ -103,47 +117,57 @@ def chi2_result_list(contingency_tables: List[ContingencyTable]) -> list:
         chi2_results.append(chi2_result)
     return chi2_results
 
-def chi2_pvalue_percentage_pass(alpha: float, chi2_results: list) -> float:
-    return len(
-        [
-            data.pvalue for data in chi2_results
-            if data.pvalue > alpha
-        ])/len([
-            data.pvalue for data in chi2_results
-            ]
-            )
 
-def add_percentages_to_bit_group(parent: h5py._hl.group.Group, contingency_tables: List[ContingencyTable]) -> None:
-    
+def chi2_pvalue_percentage_pass(alpha: float, chi2_results: list) -> float:
+    return len([data.pvalue for data in chi2_results if data.pvalue > alpha]) / len(
+        [data.pvalue for data in chi2_results]
+    )
+
+
+def add_percentages_to_bit_group(
+    parent: h5py.Group, contingency_tables: List[ContingencyTable]
+) -> None:
     percentages_per_variable = {
-        variable_name: list()
-        for variable_name in contingency_tables[0].variables
+        variable_name: list() for variable_name in contingency_tables[0].variables
     }
 
     for contingency_table in contingency_tables:
         percentage_flipped_dict = contingency_table.percentage_flipped_dict()
         for variable_name, value in percentage_flipped_dict.items():
             percentages_per_variable[variable_name].append(value)
-    
+
     for variable_name, percentage_list in percentages_per_variable.items():
         parent.create_dataset(
             f"{variable_name}_percentage_of_bits_flipped_to_1",
             (len(percentage_list),),
             dtype="f",
-            data=percentage_list
+            data=percentage_list,
         )
 
-        unstable_bits_flip_percent = [percent for percent in percentage_list if percent != 1 and percent != 0]
+        unstable_bits_flip_percent = [
+            percent for percent in percentage_list if percent != 1 and percent != 0
+        ]
         # Meta Data
-        parent.attrs[f"Percentage of stable bits ({variable_name})"] = 1 - len(unstable_bits_flip_percent)/len(percentage_list)
-        parent.attrs[f"Mean probability for unstable bits to flip to 1 ({variable_name})"] = sum(unstable_bits_flip_percent)/len(unstable_bits_flip_percent)
-        parent.attrs[f"Variance of probability for unstable bits to flip to 1 ({variable_name})"] = np.var(unstable_bits_flip_percent)
-    
+        parent.attrs[f"Percentage of stable bits ({variable_name})"] = 1 - len(
+            unstable_bits_flip_percent
+        ) / len(percentage_list)
+        parent.attrs[
+            f"Mean probability for unstable bits to flip to 1 ({variable_name})"
+        ] = sum(unstable_bits_flip_percent) / len(unstable_bits_flip_percent)
+        parent.attrs[
+            f"Variance of probability for unstable bits to flip to 1 ({variable_name})"
+        ] = np.var(unstable_bits_flip_percent)
 
 
-def add_bit_type_group(parent: h5py._hl.group.Group, read_sessions: Dict[str, ReadSession], bit_type: str) -> None:
-    contingency_tables = contingency_table_list(read_sessions, bit_type, normalize=False)
-    normalized_contingency_tables = contingency_table_list(read_sessions, bit_type, normalize=True)
+def add_bit_type_group(
+    parent: h5py.Group, read_sessions: Dict[str, ReadSession], bit_type: str
+) -> None:
+    contingency_tables = contingency_table_list(
+        read_sessions, bit_type, normalize=False
+    )
+    normalized_contingency_tables = contingency_table_list(
+        read_sessions, bit_type, normalize=True
+    )
 
     bit_chi2_results = chi2_result_list(normalized_contingency_tables)
 
@@ -153,32 +177,34 @@ def add_bit_type_group(parent: h5py._hl.group.Group, read_sessions: Dict[str, Re
         "pvalues",
         (len(bit_chi2_results),),
         dtype="f",
-        data=[result.pvalue for result in bit_chi2_results]
+        data=[result.pvalue for result in bit_chi2_results],
     )
 
-    bit_group.attrs["Percentage of bits with pvalue > alpha:=0.05"] = chi2_pvalue_percentage_pass(0.05, bit_chi2_results)
-    bit_group.attrs["Percentage of bits with pvalue > alpha:=0.1"] = chi2_pvalue_percentage_pass(0.1, bit_chi2_results)
+    bit_group.attrs["Percentage of bits with pvalue > alpha:=0.05"] = (
+        chi2_pvalue_percentage_pass(0.05, bit_chi2_results)
+    )
+    bit_group.attrs["Percentage of bits with pvalue > alpha:=0.1"] = (
+        chi2_pvalue_percentage_pass(0.1, bit_chi2_results)
+    )
+
 
 def chi2_result_to_hdf5(outpath: Path, read_sessions: Dict[str, ReadSession]) -> None:
-
     with h5py.File(outpath, "w") as f:
         add_bit_type_group(f, read_sessions, "parity_reads")
         add_bit_type_group(f, read_sessions, "data_reads")
+
 
 parser = argparse.ArgumentParser(
     "Script that takes BRAM experiment hdf5 file, unpacks the latter and does a chi squared test over the data\n"
     "The goal is to prove that certain variables do not influence bitflips in the bram"
 )
 parser.add_argument(
-    "--read_hdf5",
-    required=True,
-    help="Path to hdf5 file containing bram reads"
+    "--read_hdf5", required=True, help="Path to hdf5 file containing bram reads"
 )
 parser.add_argument(
-    "--out_hdf5",
-    required=True,
-    help="Path where result hdf5 shall be written"
+    "--out_hdf5", required=True, help="Path where result hdf5 shall be written"
 )
+
 
 def main():
     args = parser.parse_args()
@@ -186,15 +212,14 @@ def main():
 
     experiment = unpack_from_hdf5(arg_dict["read_hdf5"])
     bram_block = list(
-        list(
-            list(
-                experiment.boards.values()
-            )[0]
-            .pblocks.values()
-        )[0]
-        .bram_blocks.values()
-    )[0] # We just assume that the experiment was only performed on a single bram for this script
+        list(list(experiment.boards.values())[0].pblocks.values())[
+            0
+        ].bram_blocks.values()
+    )[
+        0
+    ]  # We just assume that the experiment was only performed on a single bram for this script
     chi2_result_to_hdf5(arg_dict["out_hdf5"], bram_block.read_sessions)
+
 
 if __name__ == "__main__":
     main()
--- tests/test_experiment_hdf5.py
+++ tests/test_experiment_hdf5.py
@@ -4,42 +4,44 @@ import h5py
 from pathlib import Path
 from hdf5_wrapper import Experiment, Read
 
+
 class TestExperiment(unittest.TestCase):
-    '''
+    """
     Light sample test on Experiment hdf5 classes.
-    '''
+    """
+
     test_hdf5_file_path = Path("tests", "test_data", "test.hdf5")
 
     def test_init(self):
-
         # Load Experiment class from test data
         with h5py.File(self.test_hdf5_file_path, "r") as f:
             experiment = Experiment.from_hdf5(f, "commit_place_holder")
 
-            bram_block = experiment.boards["te0802"].pblocks["pblock_1"].bram_blocks["RAMB36_X2Y12"]
-            
+            bram_block = (
+                experiment.boards["te0802"]
+                .pblocks["pblock_1"]
+                .bram_blocks["RAMB36_X2Y12"]
+            )
+
             self.assertEqual(len(bram_block.read_sessions), 2)
             # This is repetition of the same code block and could be refactored
             # But it shall suffice for testing (due to time management)
             self.assertEqual(
                 len(bram_block.read_sessions["previous_value_00"].data_reads),
-                len(bram_block.read_sessions["previous_value_ff"].data_reads)    
+                len(bram_block.read_sessions["previous_value_ff"].data_reads),
             )
             self.assertEqual(
-                len(bram_block.read_sessions["previous_value_00"].data_reads),
-                1000    
-            )
-            self.assertEqual(
-                len(bram_block.read_sessions["previous_value_00"].parity_reads),
-                len(bram_block.read_sessions["previous_value_ff"].parity_reads)    
+                len(bram_block.read_sessions["previous_value_00"].data_reads), 1000
             )
             self.assertEqual(
                 len(bram_block.read_sessions["previous_value_00"].parity_reads),
-                1000    
+                len(bram_block.read_sessions["previous_value_ff"].parity_reads),
             )
             self.assertEqual(
-                len(bram_block.read_sessions["previous_value_00"].temperatures),
-                1000
+                len(bram_block.read_sessions["previous_value_00"].parity_reads), 1000
+            )
+            self.assertEqual(
+                len(bram_block.read_sessions["previous_value_00"].temperatures), 1000
             )
 
             for read in bram_block.read_sessions["previous_value_00"].data_reads:
@@ -47,78 +49,74 @@ class TestExperiment(unittest.TestCase):
                 self.assertEqual(len(read.bits), 4096)
                 for byte in read.bits:
                     self.assertEqual(len(byte), 8)
-            
+
             for read in bram_block.read_sessions["previous_value_ff"].parity_reads:
                 self.assertEqual(len(read.raw_read), 512)
                 self.assertEqual(len(read.bits), 512)
                 for byte in read.bits:
                     self.assertEqual(len(byte), 8)
 
+
 class TestRead(unittest.TestCase):
     reads_homogene_00 = [
-        Read(b'\x00\x00\x00'),
-        Read(b'\x00\x00\x00'),
+        Read.from_raw(b"\x00\x00\x00"),
+        Read.from_raw(b"\x00\x00\x00"),
     ]
-    reads_homogene_ff = [
-        Read(b'\xff\xff\xff'),
-        Read(b'\xff\xff\xff')
-    ]
-    reads_heterogene = [
-        Read(b'\xff\x00\xff'),
-        Read(b'\x00\xff\x00')
-    ]
+    reads_homogene_ff = [Read.from_raw(b"\xff\xff\xff"), Read.from_raw(b"\xff\xff\xff")]
+    reads_heterogene = [Read.from_raw(b"\xff\x00\xff"), Read.from_raw(b"\x00\xff\x00")]
     reads_heterogene_similar = [
-        Read(b'\xff\xff\xff'),
-        Read(b'\xf0\x0f\xf0')
+        Read.from_raw(b"\xff\xff\xff"),
+        Read.from_raw(b"\xf0\x0f\xf0"),
     ]
 
-    def test_bits_flatted(self) -> None:
+    def test_bits_flattened(self) -> None:
         self.assertEqual(
-            [read.bits_flatted for read in self.reads_homogene_00],
+            [read.bits_flattened for read in self.reads_homogene_00],
             [
-                np.array([
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                ]),
-                np.array([
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                ])
-            ]
+                np.array(
+                    [
+                        [0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0],
+                    ]
+                ),
+                np.array(
+                    [
+                        [0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0],
+                    ]
+                ),
+            ],
         )
 
         self.assertEqual(
-            [read.bits_flatted for read in self.reads_heterogene_similar],
+            [read.bits_flattened for read in self.reads_heterogene_similar],
             [
-                np.array([
-                    [1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1]
-                ]),
-                np.array([
-                    [1, 1, 1, 1, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 0, 0, 0, 0]
-                ])
-            ]
+                np.array(
+                    [
+                        [1, 1, 1, 1, 1, 1, 1, 1],
+                        [1, 1, 1, 1, 1, 1, 1, 1],
+                        [1, 1, 1, 1, 1, 1, 1, 1],
+                    ]
+                ),
+                np.array(
+                    [
+                        [1, 1, 1, 1, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 1, 1, 1, 1],
+                        [1, 1, 1, 1, 0, 0, 0, 0],
+                    ]
+                ),
+            ],
         )
 
     def test_entropy(self) -> None:
+        self.assertEqual([read.entropy for read in self.reads_homogene_00], [0, 0])
+        self.assertEqual([read.entropy for read in self.reads_homogene_ff], [0, 0])
         self.assertEqual(
-            [read.entropy for read in self.reads_homogene_00],
-            [0, 0]
-        )
-        self.assertEqual(
-            [read.entropy for read in self.reads_homogene_ff],
-            [0, 0]
-        )
-        self.assertEqual(
-            [read.entropy for read in self.reads_heterogene_similar],
-            [0, 1]
+            [read.entropy for read in self.reads_heterogene_similar], [0, 1]
         )
         self.assertEqual(
             [read.entropy for read in self.reads_heterogene],
-            [0.9182958340544894, 0.9182958340544894]
+            [0.9182958340544894, 0.9182958340544894],
         )
