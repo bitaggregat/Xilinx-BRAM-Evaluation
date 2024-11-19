06f3ae02d2d06089d9cf9c8ec3e768bc27584e1f
Wakiya Schulz <w.schulz@bitaggregat.de>
2024-11-19 14:52:27 +0100

    Made paths in config.sh dynamic to pwd

--- config.sh
+++ config.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 vivado_path="/tools/Xilinx/Vivado/2024.1/bin/vivado"
-vivado_project_path="/home/kiya/Repos/xilinx-bram-evaluation/vivado_project/te0802/read_bram_te0802"
+vivado_project_path="${PWD}/vivado_project/te0802/read_bram_te0802"
 pblock="pblock_1"
 bram_row_x_position=2
 bram36_min_y_position=12
 bram36_max_y_position=12
 reads=10
-output_path="/home/kiya/Repos/xilinx-bram-evaluation/test_runs"
+output_path="${PWD}/test_runs"
 use_previous_value_ff="True"
 uart_sn="A503VSXV"
 programming_interface="Digilent/25163300869FA"
